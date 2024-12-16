631192c4d7d5c575799fe7aa50af58e52f795df9
Bruce-Robert Pocock <brpocock@star-hope.org>
2024-12-16 08:10:49 -0800

    ensure directories exist

--- src/graphics.lisp
+++ src/graphics.lisp
@@ -2730,7 +2730,8 @@ Columns: ~d
          (palette-pixels (png->palette height width
                                        (png-read:image-data png)
                                        α)))
-    (with-output-to-file (font.s "Source/Generated/Assets/Font.s" :if-exists :supersede)
+    (ensure-directories-exist #p"Source/Generated/Assets/Font.s")
+    (with-output-to-file (font.s #p"Source/Generated/Assets/Font.s" :if-exists :supersede)
       (format font.s ";;; Font compiled from ~a
 ;;; This is a generated file, edit the source XCF file
 
