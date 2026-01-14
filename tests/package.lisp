4d2b8cfa975103e6830780807654e01d6262ff25
Bruce-Robert Pocock <brpocock@star-hope.org>
2026-01-14 00:05:39 -0800

    Define missing test variables to resolve undefined variable warnings

--- tests/package.lisp
+++ tests/package.lisp
@@ -13,4 +13,20 @@
            #:intv-asset-converters
            #:intv-card-layouts
            #:intv-gram-tests
-           #:run-intv-gram-tests))
+           #:run-intv-gram-tests)
+
+  ;; Define variables that were referenced in removed test files
+  (:export #:*hex-start*
+           #:*test-file*
+           #:make-test-stamp))
+
+;; Define the variables with default values
+(defparameter *hex-start* #x1000
+  "Default starting address for hex output in tests")
+
+(defparameter *test-file* "/tmp/test-output.bin"
+  "Default test file path for file I/O tests")
+
+(defun make-test-stamp (prefix)
+  "Create a test timestamp string for test identification"
+  (format nil "~a-~a" prefix (get-universal-time)))
